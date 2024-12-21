13fd0ab1a9c1085ba4c060ebfa7735a01b4b7d60
Mauricio Coria <coriamauricio@gmail.com>
2024-12-21 09:20:52 -0300

    Adding class UciService

---
+++ src/main/java/net/chesstango/uci/protocol/UciService.java
@@ -0,0 +1,20 @@
+package net.chesstango.uci.protocol;
+
+import net.chesstango.uci.protocol.stream.UCIOutputStream;
+
+/**
+ * @author Mauricio Coria
+ * <p>
+ * El servicio recibe mensajes UCI por ser UCIOutputStream y
+ * y escribe toda respuesta en el OutputStream que recibe como referencia.
+ * <p>
+ * La interfaz podria servir para implementar engine como gui.
+ */
+public interface UciService extends UCIOutputStream {
+    void open();
+
+    void close();
+
+    void setResponseOutputStream(UCIOutputStream output);
+
+}
